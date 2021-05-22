6ac9b99983a29fd5064ff3ca0d53b8df235e16a9
yhnujmtgb025 <49501773+yhnujmtgb025@users.noreply.github.com>
2021-05-22 19:05:43 +0700

    Done view forget

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
   res.render('home')
 })
 app.get('/login', function (req, res) {
-    res.render('login')
-  })
- 
-app.listen(8080,() => console.log('http://localhost:8080'))
+  res.render('handleLogin/login')
+})
+app.get('/forget', function (req, res) {
+  res.render('handleLogin/forget')
+})
+app.listen(8080, () => console.log('http://localhost:8080'))
