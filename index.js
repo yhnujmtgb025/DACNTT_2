8aab89e2cafd9ba494f53f1f4ca97a561486c77d
yhnujmtgb025 <49501773+yhnujmtgb025@users.noreply.github.com>
2021-05-22 22:02:08 +0700

    Slacking

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-  res.render('home')
+  res.render('homePage/home')
 })
 app.get('/login', function (req, res) {
   res.render('handleLogin/login')
